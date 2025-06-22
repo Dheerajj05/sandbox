2f731b25022561e9f0c0cfca0d7fb820888d2926
= <=>
2025-06-22 21:00:29 +0530

    docs(events): added a method using inner html in keyboard mini project

--- js/events/05-keyboard-mini-project/script.js
+++ js/events/05-keyboard-mini-project/script.js
@@ -1,3 +1,26 @@
+// Method 1
+// window.addEventListener('keydown', (e) => {
+//   const insert = document.getElementById('insert');
+
+//   insert.innerHTML = `
+//     <div class="key">
+//       ${e.key === ' ' ? 'Space' : e.key}
+//       <small>e.key</small>
+//     </div>
+
+//     <div class="key">
+//       ${e.keyCode}
+//       <small>e.keyCode</small>
+//     </div>
+
+//     <div class="key">
+//       ${e.code}
+//       <small>e.code</small>
+//     </div>
+//   `;
+// });
+
+// Method 2
 const insert = document.getElementById('insert');
  
 const onKeyDown = (e) => {
