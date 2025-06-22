9c75a48b6f94a5d182d0ef8f7ac1f715cf1b3fa2
= <=>
2025-06-22 07:50:14 +0530

    docs(events): added mini project on keyboard events

---
+++ js/events/05-keyboard-mini-project/script.js
@@ -0,0 +1,23 @@
+const insert = document.getElementById('insert');
+ 
+const onKeyDown = (e) => {
+    const keyCodes = {
+        'e.key': e.key=== ' ' ? 'Space' : e.key,
+        'e.keyCode': e.keyCode,
+        'e.code': e.code,
+    }
+    insert.innerHTML = '';
+
+    for (const key in keyCodes) {
+        const div = document.createElement('div');
+        div.className = 'key';
+        const small = document.createElement('small');
+        const keyText = document.createTextNode(key);
+        const valueText = document.createTextNode(keyCodes[key]);
+        small.appendChild(keyText);
+        div.appendChild(small);
+        div.appendChild(valueText);
+        insert.appendChild(div);
+    }
+}
+window.addEventListener('keydown', onKeyDown);
